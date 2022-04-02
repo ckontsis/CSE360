9847a0cd9b17fb2cb9b3097fc64cd398cc3c3ab7
azincenk <98792170+azincenk@users.noreply.github.com>
2022-04-01 20:34:02 -0700

    Update Main.java

--- .github/workflows/meun/Main.java
+++ .github/workflows/meun/Main.java
@@ -15,13 +15,37 @@ import java.io.InputStream;
 
 
 public class Main extends Application {
+	
+	static volatile ArrayList<Item> menu = new ArrayList<Item>();
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
+		
 		//Parent root1 = FXMLLoader.load(getClass().getResource("popup.fxml"));
+		
+		
 		Parent root = FXMLLoader.load(getClass().getResource("Test.fxml"));
+		
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("GUI.fxml"));
+    			
+    		editController controller = new editController(menu);
+    			
+    		loader.setController(controller);
+    			
+    		AnchorPane anchor = loader.load();
+    		Scene scene = new Scene(anchor, 1055, 755);
+    			    			
+    		primaryStage.setTitle("Edit Menu");
+
+    		primaryStage.setScene(scene);
+    		primaryStage.show();
+    			
+    		this.menu = controller.getNewMenu();
+		
 		
 		primaryStage.setTitle("abc");
 		
