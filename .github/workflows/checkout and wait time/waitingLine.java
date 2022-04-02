0287f3a7fb0d86f4c3ab54ad48c418325cdf7181
davidwang7777 <76135199+davidwang7777@users.noreply.github.com>
2022-04-02 10:54:36 -0700

    Add files via upload

---
+++ .github/workflows/checkout and wait time/waitingLine.java
@@ -0,0 +1,28 @@
+package RestaurantGUI;
+import java.lang.*;
+import java.util.*;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class waitingLine implements Initializable {
+    @FXML
+    private Label Label1;
+
+    @FXML
+    private Label Label2;
+
+    @FXML
+    private Label Label3;
+
+    int peopleInLine = (int)(Math.random() * 20);
+
+    @FXML
+
+    @Override
+    public void setText() {
+        int rand = (int) (Math.random() * 20);
+        Label3.setText("                                                                 Approximate Wait Time: " + String.valueOf(rand));
+        Label1.setText("                                                                You are behind: " + String.valueOf(peopleInLine));
+    }
+}
+
