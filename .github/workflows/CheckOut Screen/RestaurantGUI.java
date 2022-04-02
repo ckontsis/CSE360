fbc8c709bde420711edcf722c0940d654dad6d0a
davidwang7777 <76135199+davidwang7777@users.noreply.github.com>
2022-04-02 10:25:49 -0700

    Add files via upload

---
+++ .github/workflows/CheckOut Screen/RestaurantGUI.java
@@ -0,0 +1,30 @@
+package RestaurantGUI;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.StackPane;
+import java.util.ArrayList;
+
+public class RestaurantGUI extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+        try {
+            BorderPane root = new BorderPane();
+            Scene scene = new Scene(root, 300, 275);
+            scene.getStylesheets().add(getClass().getResource("sample.fxml"));
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
